024e8edcdc78a3218d992fd2f43c99610c3a0b60
greekmusicaltradition <olga.lefa@gmail.com>
2025-08-17 12:05:09 +0300

    Update header.js

--- js/header.js
+++ js/header.js
@@ -24,7 +24,7 @@ function loadHeader() {
             nav: {
                 home: "Αρχική",
                 lessons: "Μαθήματα",
-                about: "Σχετικά με Εμένα",
+                about: "Σχετικά",
                 contact: "Επικοινωνία"
             }
         }
