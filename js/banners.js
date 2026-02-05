f4f7d26c789a0c701bf6e7c37079712275a2431a
greekmusicaltradition <olga.lefa@gmail.com>
2026-02-05 08:33:51 +0200

    Update banners.js

--- js/banners.js
+++ js/banners.js
@@ -1,1 +1,17 @@
+        // Back to top button functionality
+        const backToTopButton = document.getElementById('back-to-top');
 
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > 300) {
+                backToTopButton.classList.add('show');  
+            } else {
+                backToTopButton.classList.remove('show');
+            }
+        });
+
+        backToTopButton.addEventListener('click', () => {
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            });
+        });
